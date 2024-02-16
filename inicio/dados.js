66d3f63f992c16b875c9e00b1aafbf344a9d3daf
Efavil <edu.piresfavila@gmail.com>
2024-02-16 15:54:23 -0300

    começando

---
+++ inicio/dados.js
@@ -0,0 +1,17 @@
+var quantidade = 10;
+var preco = 6.4;
+var taxa = 1.50;
+let lucro = 7.32;
+let precoFinal = preco + lucro;
+let nome = "Caneta"
+
+console.log(nome)
+console.log(quantidade);
+console.log(preco);
+console.log(preco - taxa);
+console.log(preco + lucro);
+console.log(preco  * quantidade / taxa);
+console.log(precoFinal)
+
+nome = "Caneta Bic"
+console.log(nome)
