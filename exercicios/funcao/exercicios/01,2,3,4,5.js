b020f798e5dd74f692541d2609b935c890c5c922
Efavil <edu.piresfavila@gmail.com>
2024-06-25 18:56:47 -0300

    +, +, +

---
+++ exercicios/funcao/exercicios/01,2,3,4,5.js
@@ -0,0 +1,36 @@
+function trat(a, b) {
+    let soma = a + b
+    let sub = a - b
+    let mult = a * b
+    let div = a / b
+
+    return console.log(soma, sub, mult, div)
+}
+
+trat(5, 3)
+
+function classificaTriangulo (lado1, lado2, lado3) {
+    if(lado1 == lado2 && lado2 == lado3){
+        return 'Equilátero'
+    } else if (lado1 == lado2 || lado2 == lado3 || lado1 == lado3) {
+        return 'Isósceles'        
+    }else { 
+        return 'Escaleno'
+    }
+}
+
+console.log(classificaTriangulo(2, 2, 2));
+console.log(classificaTriangulo(2, 3, 3));
+console.log(classificaTriangulo(2, 3, 4));
+
+function expoente (base, expoente) {
+    //Podemos fazer de 2 modos
+    //Método antigo:
+    let resultado = Math.pow(base, expoente)
+    //Método novo:
+    resultado = base ** expoente
+
+    return resultado
+}
+
+console.log(expoente(2, 3))
