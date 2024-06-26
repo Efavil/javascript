51b3270753bcdfa778c8b746b3b22005a535d105
Efavil <edu.piresfavila@gmail.com>
2024-06-26 15:58:04 -0300

    calm

--- exercicios/obj/funcoesImportantes.js
+++ exercicios/obj/funcoesImportantes.js
@@ -11,3 +11,24 @@ console.log(Object.entries(pessoa))
 Object.entries(pessoa).forEach(([chave, valor]) => {
     console.log(`${chave}: ${valor}`)
 })
+
+Object.defineProperty(pessoa, 'dataNascimento', {
+    enumerable: true,
+    writable: false,
+    value: '01/01/2019'
+})
+
+pessoa.dataNascimento = '01/01/2017'
+console.log(pessoa.dataNascimento)
+console.log(Object.keys(pessoa))
+
+// Object.assing (ECMAScript 2015)
+const dest = { a: 1 }
+const o1 = { b: 2 }
+const o2 = { c: 3, a:4 }
+const obj = Object.assign(dest, o1, o2)
+console.log(dest)
+
+Object.freeze(obj)
+obj.c = 1234
+console.log(obj)
