463b20119c057db7dfb6690c0ba00d583a8f4246
Efavil <edu.piresfavila@gmail.com>
2024-08-18 18:21:48 -0300

    factory e construction

--- exercicios/funcao/factoryFunc.js
+++ exercicios/funcao/factoryFunc.js
@@ -2,11 +2,27 @@ function criarPessoa(nome, sobrenome, a, p) {
     return {
         nome,
         sobrenome,
-        falar(assunto) {
+
+        //Getter
+        get nomeCompleto() {
+            return `${this.nome} ${this.sobrenome}`
+        },
+
+        //Setter
+        set nomeCompleto(valor) {
+            valor = valor.split(' ');
+            this.nome = valor.shift()
+            this.sobrenome = valor.join(' ')
+        },
+
+        falar(assunto = 'falando sobre NADA') {
             return `${this.nome} está ${assunto}.`;
         },
+
         altura: a,
         peso: p,
+
+        // Getter
         get imc() {
             const indice = this.peso / (this.altura ** 2);
             return indice.toFixed(2);
@@ -15,6 +31,8 @@ function criarPessoa(nome, sobrenome, a, p) {
 }
 
 const p1 = criarPessoa('Luiz', 'Otávio', 1.8, 80);
+p1.nomeCompleto = 'Maria Oliveira Silva';
 console.log(p1.nome);
 console.log(p1.sobrenome);
-console.log(p1.imc);
+console.log(p1.falar());
+
